2f8ea9e0a5c9e331ad1a00600cb7dc59483e7920
YOURNAME <youremail@gmail.com>
2026-05-29 21:08:36 +0300

    update subscriptions

--- src/App.jsx
+++ src/App.jsx
@@ -287,11 +287,13 @@ const orderItems = cart
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 sm:gap-8">
           <div className="bg-black border border-zinc-800 p-4 md:p-8 rounded-3xl hover:-translate-y-3 hover:border-white/20 hover:shadow-2xl hover:shadow-white/10 transition-all duration-300">
+          {/* 
             <img
   src={residentialImg}
   alt="Residential"
   className="w-full h-40 md:h-auto object-cover rounded-2xl mb-4 hover:scale-105 transition duration-500"
 />
+*/}
             <h3 className="text-2xl md:text-4xl font-bold mb-4">
               Residential
             </h3>
